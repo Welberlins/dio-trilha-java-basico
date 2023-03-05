25df5865e4d33beb2a4f7895f30c60ebc151cafc
Welber Lins <welber_lins@outlook.com>
2023-03-05 11:48:42 -0300

    aula 2

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis {
+  public static void main(String[] args) throws Exception {
+    int numero = 5;
+    numero = 10;
+    System.out.print(numero);
+
+    //VALOR NAO PODE SER MUDADO
+    final double VALOR_PI = 3.14;
+    
+  }
+}
